75c5ed8b85bd954e173e27d73f5dabedb1cc6922
yix-shu <yix.shu723@gmail.com>
2022-07-31 16:25:06 -0700

    Cloning tumbleweed and destroying current tumbleweed when reaches wall

--- Assets/Scripts/Tumble.cs
+++ Assets/Scripts/Tumble.cs
@@ -7,19 +7,33 @@ namespace Assets.Scripts
     {
         private Rigidbody2D rb;
 
+        private CircleCollider2D cCollider;
+
         [SerializeField]
         private float speed = 2f;
+
+        [SerializeField]
+        private Transform spawn;
 
         // Use this for initialization
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
+            spawn = GetComponent<Transform>();
         }
 
         // Update is called once per frame
         void Update()
         {
             rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.name == "Wall")
+            {
+                Object.Instantiate(this.gameObject, spawn);
+                Object.Destroy(this.gameObject);
+            }
         }
     }
 }
