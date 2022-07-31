ebd0613d21ad4fea61fd5d4b7bf910cb4ab70e85
yix-shu <yix.shu723@gmail.com>
2022-07-31 16:30:36 -0700

    Added dampening to speed when colliding with ground and created prefab for easier cloning

--- Assets/Scripts/Tumble.cs
+++ Assets/Scripts/Tumble.cs
@@ -5,35 +5,42 @@ namespace Assets.Scripts
 {
     public class Tumble : MonoBehaviour
     {
+        public GameObject tumblePrefab;
+
         private Rigidbody2D rb;
 
-        private CircleCollider2D cCollider;
+        [SerializeField]
+        private float speed = 20.0f;
 
         [SerializeField]
-        private float speed = 2f;
+        private Vector3 spawn = new Vector3(-97.2f, 1.0f, -4111.0f);
 
-        [SerializeField]
-        private Transform spawn;
+        private float rotateSpeed = -0.3f;
 
         // Use this for initialization
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
-            spawn = GetComponent<Transform>();
         }
 
         // Update is called once per frame
         void Update()
         {
             rb.velocity = new Vector2(speed, rb.velocity.y);
+            this.gameObject.transform.Rotate(0.0f, 0.0f, rotateSpeed);
+            speed = Random.Range(20, 50);
         }
-        private void OnCollisionEnter(Collision collision)
-        {
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        { 
             if (collision.gameObject.name == "Wall")
             {
-                Object.Instantiate(this.gameObject, spawn);
+                Debug.Log("Collided");
+                Instantiate(tumblePrefab, spawn, Quaternion.identity);
                 Object.Destroy(this.gameObject);
             }
+            rotateSpeed = Random.Range(-0.1f, -0.3f);
+            speed += 10.0f;
         }
     }
 }
