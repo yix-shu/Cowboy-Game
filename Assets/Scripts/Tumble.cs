e2a6e7b5cca76a64dfe63269c613ee4f1fe13c58
yix-shu <yix.shu723@gmail.com>
2022-07-31 15:32:51 -0700

    Creating tumble script for tumbleweed animation in background

---
+++ Assets/Scripts/Tumble.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Tumble : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+
+        [SerializeField]
+        private float speed = 2f;
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+    }
+}
