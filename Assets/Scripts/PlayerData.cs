84802cc6fbd7777d943a4c4f235722520472c043
yix-shu <yix.shu723@gmail.com>
2022-07-30 19:22:12 -0700

    Creating the save system which will use a binary formatter to serialize player data

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerData : MonoBehaviour
 {
     public int exp;
     public int money;
+
+    public PlayerData(Player player)
+        level = player.level;
+        money = player.money;
+
 }
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary; //Binary Formatter for translating data
+
+namespace Assets.Scripts
+{
+    public static class SaveSystem 
+    {
+        public static void SavePlayer(Player player)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Application.persistentDataPath + "/player.fun"; //Application.persistentDataPath uses the OS's system file path
+
+        }
+    }
+}
