46281acf385af535d8da33cd2d6bf74729ed1d34
yix-shu <yix.shu723@gmail.com>
2022-07-30 19:27:10 -0700

    Save system now saves and loads player data with binary formatter and using file io

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -10,7 +10,31 @@ namespace Assets.Scripts
         {
             BinaryFormatter formatter = new BinaryFormatter();
             string path = Application.persistentDataPath + "/player.fun"; //Application.persistentDataPath uses the OS's system file path
+            FileStream stream = new FileStream(path, FileMode.Create);
 
+            PlayerData data = new PlayerData(player);
+
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+
+        public static PlayerData LoadPlayer()
+        {
+            string path = Application.persistentDataPath + "/player.fun";
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                PlayerData data = formatter.Deserialize(stream) as PlayerData;
+
+                return data;
+            }
+            else
+            {
+                Debug.LogError("Save file not found in " + path);
+                return null;
+            }
         }
     }
 }
