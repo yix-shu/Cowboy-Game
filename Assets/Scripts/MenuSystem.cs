fb7b498375afe009febce19382f9fbdc94505b6d
yix-shu <yix.shu723@gmail.com>
2022-08-07 22:35:13 -0700

    Successfully loads player's saved data. Added debug logging

--- Assets/Scripts/MenuSystem.cs
+++ Assets/Scripts/MenuSystem.cs
@@ -6,9 +6,29 @@ namespace Assets.Scripts
 {
     public class MenuSystem : MonoBehaviour
     {
+        public Player player;
+        /*
+        void Start()
+        {
+            Debug.Log(player.money);
+            Debug.Log(player.exp);
+        }
+        */
         public void Play()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        public void Continue()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerData data = SaveSystem.LoadPlayer();
+
+            player.money = data.money; 
+            player.exp = data.exp;
+
+            Debug.Log(player.money);
+            Debug.Log(player.exp);
+            
         }
     }
 }
