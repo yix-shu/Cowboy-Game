b0d4f5af008e9986ed591af5e0ab6c9c3a7840af
yix-shu <yix.shu723@gmail.com>
2022-08-14 16:03:02 -0700

    Created the CostumeObject class

--- Assets/Scripts/CostumeObject.cs
+++ Assets/Scripts/CostumeObject.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+[CreateAssetMenu(fileName = "New Costume Object", menuName = "Inventory System/Items/Default")]
 public abstract class CostumeObject : ItemObject
 {
-
+	public void Awake()
+    {
+        typeof = ItemType.Costume;
+    }
 }
 
